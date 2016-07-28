0786cbc1bf0ef82df683f797e5833bc7c876f4f2
Chris' Mac <chris@MacBook.local>
2016-07-28 01:24:07 -0700

    Added VS Code debugging project

---
+++ samples/VSCodeDebugging/Program.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace VSCodeDebugging
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            string motd = "Hello World!";
+            Console.WriteLine("MOTD: {0}", motd);
+        }
+    }
+}
